94e74f87e7bbd05846dc194e198640e1aabb4185
KiandroDW <kiandro.dewaegenaere@ugent.be>
2025-04-06 16:41:58 +0200

    Escape now quits the program

--- main.py
+++ main.py
@@ -94,6 +94,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     for body in bodies:
         body.update_speed()
